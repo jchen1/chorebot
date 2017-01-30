6b36957b68987f1d516cf0add153a71dd4b4b78f
Jeff Chen <jeff@blendlabs.com>
2017-01-29 17:54:39 -0800

    .

--- src/app.ts
+++ src/app.ts
@@ -1,14 +1,22 @@
 'use strict';
 
 import * as _ from 'lodash';
-import * as express from 'express';
 
-const app = express();
+const { RtmClient, CLIENT_EVENTS } = require('@slack/client');
+const { SLACK_API_TOKEN } = require('../keys.json');
 
-app.get('/', (req, res) => {
-  res.status(200).send('<html><body>chorebot home</body></html>');
+const rtm = new RtmClient(SLACK_API_TOKEN);
+
+const channel = 'C3XV8HRNX';
+
+rtm.on(CLIENT_EVENTS.RTM.AUTHENTICATED, data => {
+  console.log(`Logged in as ${data.self.name} of team ${data.team.name}, but not yet connected`);
+  console.log(data);
 });
 
-app.listen(3000, () => {
-  console.log('chorebot listening on port 3000...');
+rtm.on(CLIENT_EVENTS.RTM.RTM_CONNECTION_OPENED, () => {
+  console.log(`Connected!!!`);
+  rtm.sendMessage('test', channel);
 });
+
+rtm.start();
